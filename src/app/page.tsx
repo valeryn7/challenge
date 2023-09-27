a0261e8700f713e139fe29583e1e572413c90efa
valerynaterchocho@gmail.com <valerynaterchocho@gmail.com>
2023-09-27 16:15:13 -0300

    home page with welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import React from "react";
 import styles from "./style.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.h1}>Hello World</h1>
+      <div className={styles.containerImage}>
+        <div className={styles.image}>
+          <div className={styles.text}>Welcome to Challenge</div>
+        </div>
+      </div>
+      <div> This is a sample application for the Frontend Challenge.</div>
     </main>
   );
 }
