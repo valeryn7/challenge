450474782b58d535b6ca006a75d07584a45fda7c
valerynaterchocho@gmail.com <valerynaterchocho@gmail.com>
2023-09-27 16:14:43 -0300

    config react icons added and eslint configuration changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,35 @@
-const config = {
-  browser: true,
-  es2021: true,
-  extends: [
-    "plugin:react/recommended",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json", "./.eslintrc.js"],
-  },
-  plugins: ["react", "prettier"],
-  rules: {"@typescript-eslint/triple-slash-reference": "off"},
-};
-module.exports = config;
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+    }
+}
